08f58736382696fb6c0e28866b1eb3c2b3ee28ea
PSCBUS_107 <pscbus_107@PSCBUS-107.local>
2020-08-30 16:37:53 -0400

    w6 d3 hw

--- student_examples/1k-divs/js/script.js
+++ student_examples/1k-divs/js/script.js
@@ -1,6 +1,25 @@
 function renderDivs() {
     // your code here
-    console.log('testing');
+    //console.log('testing');
+    //const body = document.getElementById('body')
+    //console.log(body);
+    const colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
+		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
+		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
+		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
+		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+    for(let i = 1; i < 1000; i++) {
+        const div = document.createElement('div');
+        const random = Math.floor(Math.random() * colorArray.length -1);
+        div.style.backgroundColor = colorArray[random];
+        //console.log(div);
+        document.body.appendChild(div);
+    }
 }
 
 renderDivs();
